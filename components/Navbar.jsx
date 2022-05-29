3fc28c2316fda48eaf1741a2f4b708f63e9201a8
Aymen Ouerghui <aymen.ouerghui@esprit.tn>
2022-05-29 17:53:16 +0200

    finish navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,44 @@
-import styles from "../styles/Navbar.module.css"
+import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
 
 function Navbar() {
   return (
-    <div className={styles.container} >
-      Navbar</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image
+            src="/img/telephone.png"
+            width="32"
+            height="32"
+            alt="telephone"
+          />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>+216 53 059 655</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="logo" width="160px" height="69px" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="cart" width="30" height="30" />
+
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
+ 
