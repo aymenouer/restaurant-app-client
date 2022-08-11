5cae2a5b100c37f25222ee70c493ca57b3a474d0
Aymen Ouerghui <aymen.ouerghui@esprit.tn>
2022-08-11 22:29:40 +0200

    use redux in cart

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,10 @@
 import styles from "../styles/Navbar.module.css";
 import Image from "next/image";
-
+import { useSelector } from "react-redux"
+import  Link  from 'next/link';
 function Navbar() {
+  const quantity = useSelector(state => state.cart.quantity);
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -29,13 +32,14 @@ function Navbar() {
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
+      <Link href="/cart" passHref >
       <div className={styles.item}>
         <div className={styles.cart}>
           <Image src="/img/cart.png" alt="cart" width="30" height="30" />
-
-          <div className={styles.counter}>2</div>
+          <div className={styles.counter}>{quantity}</div>
         </div>
       </div>
+      </Link>
     </div>
   );
 }
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import styles from "../../styles/Product.module.css";
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux"
+import { addProduct } from "../../redux/cartSlice";
 const Product = ({ pizza }) => {
   const [size, setSize] = useState(0);
   const [quantity, setQuantity] = useState(1);
   const [price, setPrice] = useState(pizza.prices[0]);
   const [extras, setExtras] = useState([]);
+  const dispatch = useDispatch();
 
   const changePrice = (number) => {
     setPrice(price + number);
@@ -25,6 +28,10 @@ const Product = ({ pizza }) => {
       changePrice(-option.price);
       setExtras(extras.filter(extra => extra._id !== option._id));
     }
+  };
+
+  const handleClick = () =>{
+    dispatch(addProduct({...pizza,extras,price,quantity}));
   };
 
   return (
@@ -72,7 +79,7 @@ const Product = ({ pizza }) => {
         </div>
         <div className={styles.add}>
           <input onChange={(e)=>setQuantity(e.target.value)} type="number" defaultValue={1} className={styles.quantity} />
-          <button className={styles.button}>Add to Cart</button>
+          <button className={styles.button} onClick={handleClick} >Add to Cart</button>
         </div>
       </div>
     </div>
